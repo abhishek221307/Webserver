0bcd0d33a8d594496a8779bf71236e7b8da0c4e7
Abhishek Mishra <abhishekmishraei2004@gmail.com>
2025-07-11 14:49:41 +0530

    first commit

---
+++ module/azurerm_bastion/variable.tf
@@ -0,0 +1,27 @@
+variable "bastion_name" {
+  type        = string
+  
+}
+variable "location" {
+  type        = string
+  
+}
+variable "rg_name" {
+  type        = string
+}
+
+variable "public_ip" {
+  type        = string
+  
+}
+variable "bastion-subnet" {
+  type        = string
+  
+}
+variable "virtual_network_name" {
+  type        = string
+  
+}
+
+
+
